09e18378ee7ae6ce480baf0eec586da83a758e17
MichaelHabermas <michaelghabermas@gmail.com>
2021-06-15 15:58:53 -0400

    wip

---
+++ api/auth/token-builder.js
@@ -0,0 +1,16 @@
+// jsonwebtoken
+const jwt = require('jsonwebtoken');
+
+const { JWT_SECRET } = require('../../config/secrets');
+
+module.exports = function (user) {
+	const payload = {
+		subject: user.id,
+		username: user.username,
+		role: user.role
+	};
+	const options = {
+		expiresIn: '1d'
+	};
+	return jwt.sign(payload, JWT_SECRET, options);
+};
