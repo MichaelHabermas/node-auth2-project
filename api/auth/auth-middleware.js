5bf78fb4805ca8a11e72f3e003626d2880076438
MichaelHabermas <michaelghabermas@gmail.com>
2021-06-15 15:58:40 -0400

    wip

--- api/auth/auth-middleware.js
+++ api/auth/auth-middleware.js
@@ -1,15 +1,29 @@
+const jwt = require('jsonwebtoken');
 const { JWT_SECRET } = require('../secrets'); // use this secret!
+const Users = require('../users/users-model');
 
 const restricted = (req, res, next) => {
-	next();
-
+	const token = req.headers.authorization;
+	if (token) {
+		jwt.verify(token, JWT_SECRET, (err, decoded) => {
+			if (err) {
+				next({ status: 401, message: 'Token invalid' });
+			} else {
+				console.log('decoded token: ', decoded);
+				req.decodedJwt = decoded;
+				next();
+			}
+		});
+	} else {
+		next({ status: 401, message: 'Token required' });
+	}
 	/* If the user does not provide a token in the Authorization header:
-    status 401 { "message": "Token required" }
-
-    If the provided token does not verify:
-    status 401 { "message": "Token invalid" }
-
-    Put the decoded token in the req object, to make life easier for middlewares downstream! */
+  status 401 { "message": "Token required" }
+  
+  If the provided token does not verify:
+  status 401 { "message": "Token invalid" }
+  
+  Put the decoded token in the req object, to make life easier for middlewares downstream! */
 };
 
 const only = role_name => (req, res, next) => {
@@ -21,14 +35,26 @@ const only = role_name => (req, res, next) => {
     Pull the decoded token from the req object, to avoid verifying it again! */
 };
 
-const checkUsernameExists = (req, res, next) => {
-	next(0);
+const checkUsernameExists = async (req, res, next) => {
+	const { username } = req.body;
+	const user = await Users.findBy({ username }).first();
+	if (user) {
+		next();
+	} else {
+		next({ status: 401, message: 'Invalid credentials' });
+	}
 	/* If the username in req.body does NOT exist in the database
     status 401 { "message": "Invalid credentials" } */
 };
 
-const validateRoleName = (req, res, next) => {
-	next();
+const validateRoleName = async (req, res, next) => {
+	const { role_name } = req.body;
+	const role = await Users.findBy({ role_name }).first();
+	if (role) {
+		next();
+	} else {
+		next({ status: 401, message: 'Invalid credentials' });
+	}
 	/* If the role_name in the body is valid, set req.role_name to be the trimmed string and proceed.
 
     If role_name is missing from req.body, or if after trimming it is just an empty string,
